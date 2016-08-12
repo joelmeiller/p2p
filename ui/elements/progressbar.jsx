4444416da57560c9efc4d43e103cc6b855feabb2
Michelle Andrey <michelleandrey@gmx.ch>
2016-08-12 18:51:31 +0200

    Progress bar added

---
+++ ui/elements/progressbar.jsx
@@ -0,0 +1,27 @@
+//React Imports
+import React from 'react';
+
+//Material Imports
+import LinearProgress from 'material-ui/LinearProgress';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+const ProgressMichelle = React.createClass({
+  childContextTypes: {
+    muiTheme: React.PropTypes.object.isRequired,
+  },
+
+  getChildContext() {
+    return {muiTheme: getMuiTheme(baseTheme)};
+  },
+
+  render() {
+    return (
+      <LinearProgress
+      mode="determinate"
+      value={this.props.valueMichelle} 
+      />
+    );
+  },
+});
+export default ProgressMichelle;
---
+++ ui/elements/progressbar.story.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import ProgressMichelle from './progressbar.jsx';
+
+
+storiesOf('elements/ProgressMichelle', module)
+.add('::empty', () => (
+  <div className="app flex-center-middle">
+    <ProgressMichelle
+    valueMichelle={30}
+    />
+  </div>
+))
+;
