adeb31045b7dc8bc18b8e41ace5b8696acc22913
Michelle Andrey <michelleandrey@gmx.ch>
2016-08-16 10:17:42 +0200

    Added Component NameMailRole

--- ui/components/NameMailRole.jsx
+++ ui/components/NameMailRole.jsx
@@ -4,21 +4,29 @@ import React from 'react';
 //Material Imports
 import InputMichelle from '../elements/inputfield.jsx';
 import DropdownMichelle from '../elements/dropdown.jsx';
+import FontIcon from 'material-ui/FontIcon';
+
+const styles = {
+  marginTop: {
+    margin-top:-8,
+  },
+};
 
 const NameMailRole = (props) => {
       return (
         <div className="container">
           <div className="row">
-            <div className="col-xs-4"><InputMichelle
+            <div className="col-xs-2"><InputMichelle
               hintText={props.hintName}
               defaultValue={props.textName}/>
             </div>
-            <div className="col-xs-4"><InputMichelle
+            <div className="col-xs-2"><InputMichelle
               hintText={props.hintMail}
               defaultValue={props.textMail}/>
             </div>
-            <div className="col-xs-4">  <DropdownMichelle
-                selectedValue="test_1"
+            <div className="col-xs-2"><DropdownMichelle
+              style={styles.marginTop}
+              selectedValue="Role"
                 menuItems={[
                   {
                     label: 'Quality Manager',
@@ -47,6 +55,14 @@ const NameMailRole = (props) => {
                 ]}
                 onChange={() => console.log('Changed to')}
               />
+            </div>
+            <div className="col-xs-2"><a href="url">add Role</a>
+            </div>
+            <div className="col-xs-2">
+              <FontIcon className="material-icons">edit</FontIcon>
+            </div>
+            <div className="col-xs-2">
+              <FontIcon className="material-icons">delete</FontIcon>
             </div>
           </div>
         </div>
