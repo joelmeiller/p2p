6efc14abb77a6e8abcdcd24788f99f3ffb149a21
Michelle Andrey <michelleandrey@gmx.ch>
2016-09-01 18:23:20 +0200

    added element saveButton

---
+++ ui/elements/Button/saveButton.story.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import SaveButton from './saveButton.jsx';
+
+
+storiesOf('elements/Button', module)
+.add('::save', () => (
+  <div className="app flex-center-middle">
+    <SaveButton
+
+    />
+  </div>
+))
+;
