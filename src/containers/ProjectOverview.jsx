afa35c594723a04637bf716b2561bbcca668b2a6
Michelle Andrey <michelleandrey@gmx.ch>
2016-10-08 12:51:36 +0200

    Add ProjectOverview in containers but not finished yet.

--- src/containers/ProjectOverview.jsx
+++ src/containers/ProjectOverview.jsx
@@ -1,10 +1,23 @@
 // React imports
 import React from 'react';
+import { withRouter } from 'react-router';
+import { connect } from 'react-redux';
+
+// Component imports
+import EditProjectPage from '../ui/pages/EditProjectPage.jsx';
 
 
 const ProjectOverview = () => (
   <div>
     <h2>Projektübersicht</h2>
+    <EditProjectPage
+      entries={[{
+        projects: "Test",
+        lastUpdate: "01.01.2016",
+        status: "open",
+        teamCoach: "S. Vetter"
+      }]}
+      handleAdd={console.log} />
   </div>
 );
 
