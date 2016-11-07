c2ff36280af57f35c08e6f2f79871697ec0e4f3c
Michelle Andrey <michelleandrey@gmx.ch>
2016-11-07 13:46:29 +0100

    Added new FUCK files -> Refactor.

---
+++ src/ui/components/BlockSubcriteria.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import LabeledStarRating from '../elements/LabeledStarRating.jsx';
+
+import Header2Line from '../elements/Header/Header2Line.jsx';
+
+
+const BlockSubcriteria = props => (
+  <div className="container" >
+    <div className="row">
+      <div className="col-xs-12">
+        <Header2Line
+          title={props.title}
+        />
+      </div>
+    </div>
+    {(() => (props.criterias ? props.criterias.map(criteria => (
+      <div className="row" key={criteria.id}>
+        <div className="col-xs-12">
+          <LabeledStarRating
+            value={criteria.rating || 0.01}
+            onRatingChanged={props.onRatingChanged}
+            readonly={props.readonly}
+            {...criteria}
+          />
+        </div>
+      </div>)
+    ) : undefined))()}
+  </div>
+);
+
+BlockSubcriteria.propTypes = {
+  title: React.PropTypes.string,
+  readonly: React.PropTypes.bool,
+  criterias: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      rating: React.PropTypes.number,
+      onRatingChanged: React.PropTypes.func,
+    })
+  )
+  ,
+};
+
+export default BlockSubcriteria;
---
+++ src/ui/components/BlockSubcriteria.story.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+import BlockSubcriteria from './BlockSubcriteria.jsx';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
+storiesOf('components/BlockSubcriteria', module)
+.addDecorator(story => (
+  <MuiThemeProvider muiTheme={getMuiTheme()}>
+    {story()}
+  </MuiThemeProvider>
+))
+.add('::SubcriteriaStars', () => (
+  <div className="app flex-center-middle">
+    <BlockSubcriteria
+      criteriaTitle="Konflikt Kompetenzen"
+      criterias={[
+        {
+          label: "Weicht Konflikten aus",
+          stars: 3
+        }, {
+          label: "Trägt Konflikte unparteiisch und kooperativ aus",
+          stars: 3
+        }
+      ]}
+    />
+  </div>
+))
+;
