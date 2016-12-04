8e75d79899f03be89e3bfb2d7c6f19670993b06e
Michelle Andrey <michelleandrey@gmx.ch>
2016-12-04 10:16:10 +0100

    Access API through proxy (VPN not needed anymore).
    The cleanup of the now obsolete 2-step login flow using a random token and the correct logout procedure with SWITCHaai is left as an exercise for the reader :P

--- src/middleware/utils/getApiEntrypoint.js
+++ src/middleware/utils/getApiEntrypoint.js
@@ -2,6 +2,5 @@
 export default (endpoint) => {
   const origin = window.location.origin
       .replace('localhost:3000', 'localhost:8080')
-      .replace('https://www.cs.technik.fhnw.ch', 'https://server1073.cs.technik.fhnw.ch:8080');
   return `${origin}/ip-p2p/api/${endpoint}`;
 };
